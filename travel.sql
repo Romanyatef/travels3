63dbf3319ca40dae0d0d74cf6c6e083ee76d5bc8
Romanyatef <105505073+Romanyatef@users.noreply.github.com>
2023-11-01 15:39:38 +0200

    u6

--- travel.sql
+++ travel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 01, 2023 at 02:23 PM
+-- Generation Time: Nov 01, 2023 at 02:39 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
